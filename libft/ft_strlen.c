e4c48123bd0c1a3e00b2c77d6782729d7f533bb5
RicardoMatosR <ricardomatosromero04@gmail.com>
2026-05-22 00:08:43 +0200

    subierndo actualizaciones

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -1,9 +1,11 @@
-int	ft_strlen(char *str)
+#include "libft.h"
+
+size_t	ft_strlen(const char *str)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (str)
+	while (str[i])
 		i++;
 	return (i);
 }
---
+++ libft/ft_strrchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rimatos- <rimatos-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 18:46:35 by rimatos-          #+#    #+#             */
+/*   Updated: 2026/05/21 18:46:35 by rimatos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	len;
+
+	len = ft_strlen(s);
+	while (len >= 0)
+	{
+		if (s[len] == (char)c)
+			return ((char *)&s[len]);
+		len--;
+	}
+	return (NULL);
+}
+
+#include <string.h>
+int main(void)
+{
+    strrchr()
+    return(0);
+}
+
